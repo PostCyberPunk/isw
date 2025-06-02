4907f927ed21d3da8e85305a18c58d8ac18f84fe
PostCyberPunk <lyhguest2@outlook.com>
2025-06-02 17:59:55 +0800

    add module

--- flake.nix
+++ flake.nix
@@ -9,11 +9,11 @@
   outputs = inputs @ {self, ...}:
     inputs.flake-utils.lib.eachDefaultSystem (
       system: let
+        #FIX: bab
         pkgs = import (inputs.nixpkgs) {
           inherit system;
         };
-      in {
-        packages.default = pkgs.stdenv.mkDerivation {
+        pacakge = pkgs.stdenv.mkDerivation {
           pname = "isw";
           version = "1.10";
 
@@ -34,14 +34,11 @@
                   mkdir -p $out/etc
                   mkdir -p $out/etc/modprobe.d
                   mkdir -p $out/etc/modules-load.d
-                  mkdir -p $out/usr/lib/systemd/system
-                  mkdir -p $out/usr/bin
                   mkdir -p $out/bin
 
-                  install -m755 etc/isw.conf $out/etc
-                  install -m644 etc/modprobe.d/isw-ec_sys.conf $out/etc/modprobe.d/isw-ec_sys.conf
-                  install -m644 etc/modules-load.d/isw-ec_sys.conf $out/etc/modules-load.d/isw-ec_sys.conf
-                  install -m644 usr/lib/systemd/system/isw@.service $out/usr/lib/systemd/system
+                  install -m600 etc/isw.conf $out/etc
+                  install -m600 etc/modprobe.d/isw-ec_sys.conf $out/etc/modprobe.d/isw-ec_sys.conf
+                  install -m600 etc/modules-load.d/isw-ec_sys.conf $out/etc/modules-load.d/isw-ec_sys.conf
                   install -m755 isw $out/bin
             runHook postInstall
           '';
@@ -52,6 +49,59 @@
             maintainers = [PostCyberPunk];
             platforms = platforms.linux;
             homepage = "https://github.com/YoyPa/isw";
+          };
+        };
+      in {
+        packages.default = pacakge;
+        nixosModules.default = {
+          config,
+          lib,
+          ...
+        }: let
+          cfg = config.services.isw;
+        in {
+          options = {
+            services.isw = with lib; {
+              enable = mkEnableOption "isw";
+              section = mkOption {
+                default = "";
+                type = types.str;
+                example = "16Q4EMS1";
+                description = "Section name";
+              };
+            };
+          };
+          config = lib.mkIf cfg.enable {
+            environment.systemPackages = [pacakge];
+            environment.etc = {
+              "isw.conf" = {
+                source = "${pacakge}/etc/isw.conf";
+                mode = "0600";
+              };
+              "modprobe.d/isw-ec_sys.conf" = {
+                source = "${pacakge}/etc/modprobe.d/isw-ec_sys.conf";
+                mode = "0600";
+              };
+              "modules-load.d/isw-ec_sys.conf" = {
+                source = "${pacakge}/etc/modules-load.d/isw-ec_sys.conf";
+                mode = "0600";
+              };
+            };
+            systemd.services.isw = {
+              unitConfig = {
+                Description = "ISW fan control service";
+                After = ["sleep.target"];
+              };
+
+              serviceConfig = lib.mkIf (cfg.section != "") {
+                ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
+                ExecStart = "${pacakge}/bin/isw -w ${cfg.section}";
+                Type = "oneshot";
+              };
+
+              wantedBy = ["multi-user.target" "sleep.target"];
+            };
+            ######end of config
           };
         };
       }
